26b03baf08d7a0ec04cbb9776bca620314a8290d
yeswanth <yeswanth15750@gmail.com>
2021-08-12 22:01:04 +0530

    registration and login page complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,26 @@ export class HomeComponent implements OnInit {
   // image slider java script start
 
 
-
+  slideIndex = 0;
+   showSlides() {
+  
+      let slideIndex=this.slideIndex++;
+  
+      let mySlidesDiv = document.querySelectorAll('.mySlides');
+      // Don't display images while loading
+      mySlidesDiv.forEach(divObj => {
+          // divObj.style.display = "none";
+      });
+  
+      //reset to 1st image
+      if (slideIndex > mySlidesDiv.length) {
+          slideIndex = 1
+      }
+      //display one image at at time
+      // mySlidesDiv[slideIndex - 1].style.display = "block";//displaying the image
+      setTimeout(this.showSlides, 1650); // Change image every 1.65 seconds  
+  }
+  
   //=====================================================================================================
 
 }
